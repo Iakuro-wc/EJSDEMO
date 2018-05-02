88ea615d6d5c060855209fff2eb868245c8f95dd
Iakuro-wc <wcdevcontent@gmail.com>
2018-05-01 20:08:45 -0700

    added npm express nodemon and app.listen and app.get the home route

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 //message
+
+var express = require('express');
+var app = express();
+
+app.get("/", (req, res) => {
+	res.send('Yooooooo');
+});
+
+app.listen(3000, () => {
+	console.log("listening to port 3000");
+})
